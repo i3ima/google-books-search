156c701cd38a5a034bdcab9cd7f0306c779634c9
i3ima <hueputalo.rubin@gmail.com>
2021-08-17 20:34:40 +0300

    feat(core): config file

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const DEV = import.meta.env.MODE === 'development'
+// Useless for now
+// export const GOOGLE_API_URL = import.meta.env.VITE_GOOGLE_API_URL
+// export const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
