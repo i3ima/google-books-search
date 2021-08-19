1a25cbe41eb383e8bc1f624bb24c4feba6b0f5af
i3ima <hueputalo.rubin@gmail.com>
2021-08-19 10:12:09 +0300

    refactor(config): remove useless sections of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,5 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        react: {
-          DEFAULT: '#61dafb'
-        }
-      },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
-    },
-  },
-  variants: {
-    extend: {
-      animation: ['motion-safe']
-    },
-  },
   plugins: [],
 }
