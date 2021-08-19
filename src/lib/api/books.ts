fd0455f3e6eecc2a463a62003a3a267b9ecf4cb4
i3ima <hueputalo.rubin@gmail.com>
2021-08-19 19:31:45 +0300

    feat(utils): add func for making queries

--- src/lib/api/books.ts
+++ src/lib/api/books.ts
@@ -1,7 +1,15 @@
-import { SearchOptions } from "../../types";
+import { BookCategories, SearchOptions } from "types";
 
-
+// TODO: rework this plz
 export const fetchBooks = (params: SearchOptions) =>
 		fetch(
-		`/api/volumes?q=${params.name}&startIndex=${params.page * 30}&maxResults=${30}&orderBy=${params.sortBy}`
+				`/api/volumes?q=${params.name}` +
+				`${params.category === BookCategories.ALL ? '' : `+subject:${params.category.toLowerCase()}`}` +
+				`&startIndex=${params.page * 30}&maxResults=${30}&orderBy=${params.sortBy}`, {
+				 headers: {
+				  'Content-Security-Policy': "default-src 'none'",
+					'X-Content-Security-Policy': "default-src 'none'"
+				 },
+				 mode: 'cors'
+				}
 		).catch((e) => e)
