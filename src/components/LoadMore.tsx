8bc4ede46dbc0df7fbf75c345f9a4ba19dda5991
i3ima <hueputalo.rubin@gmail.com>
2021-08-19 10:00:11 +0300

    fix(components): disable load more if pending & show spin

--- src/components/LoadMore.tsx
+++ src/components/LoadMore.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import Button from "components/ui/buttons/Button";
 import { useAppDispatch, useAppSelector } from "lib/hooks";
 import { loadMoreByOptions } from "features/books/booksSlice";
+import { AiOutlineLoading } from 'react-icons/ai'
 
 const LoadMore: React.FC = () => {
- const fetchedBooks = useAppSelector(state => state.books.fetchedBooks)
+ const [fetchedBooks, total, page, loading] = useAppSelector(state => [state.books.fetchedBooks, state.books.totalItems,
+	state.books.searchOptions.page, state.books.loading])
  const dispatch = useAppDispatch()
 
- if (fetchedBooks.length < 1) return null
+ if (fetchedBooks.length < 1 || page * 30 > total) return null
 
  return (
-     <Button onClick={() => dispatch(loadMoreByOptions())}>
-      Load More
-     </Button>
+		 <Button disabled={loading === 'pending'} className="w-full flex justify-center" onClick={() => dispatch(loadMoreByOptions())}>
+			{loading === 'pending' ? <AiOutlineLoading className="origin-center animate-spin h-8 w-8" /> : 'Load More'}
+		 </Button>
  );
 };
 
