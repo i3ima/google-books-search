2ded62a43292b6d221ac465c5777af29723f3af5
i3ima <hueputalo.rubin@gmail.com>
2021-08-19 10:09:19 +0300

    refactor(components): on key press logic now belongs to search form

--- src/components/ui/buttons/IconButton.tsx
+++ src/components/ui/buttons/IconButton.tsx
@@ -5,17 +5,10 @@ interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
  icon: JSX.Element
 }
 
-const IconButton: React.FC<IProps> = ({ icon, ...props }) => {
-
- const onEnterPress = (event: React.KeyboardEvent<HTMLButtonElement>) => {
-  return event.key === 'enter' ? event.currentTarget.click() : null
- }
-
- return (
-		 <StyledButton onKeyPress={(event => onEnterPress(event))} {...props}>
-			{icon}
-		 </StyledButton>
- )
-}
+const IconButton: React.FC<IProps> = ({ icon, ...props }) => (
+				<StyledButton {...props}>
+				 {icon}
+				</StyledButton>
+		)
 
 export default IconButton
