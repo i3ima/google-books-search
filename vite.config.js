eb7d2117a2c84b0aa7dcc0f7cf65a71a99f4ebeb
i3ima <hueputalo.rubin@gmail.com>
2021-08-19 05:16:06 +0300

    feat(core): reverse-proxy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import tsconfigPaths from "vite-tsconfig-paths";
 
+require('dotenv').config()
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()]
+  plugins: [reactRefresh(), tsconfigPaths()],
+  server: {
+    fs: {
+      allow: ['..']
+    },
+    proxy: {
+      '/api': {
+        target: 'https://www.googleapis.com/books/v1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') + `&key=${process.env.GOOGLE_API_KEY}`
+      },
+    }
+  }
 })
